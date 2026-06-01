93905c598889fa6c83eb515f33b63e5d00bb6a10
Alexander Nilsson <error_alex@hotmail.com>
2026-06-01 12:36:17 +0200

    fix(electron): resolve empty grey stage rendering issue by updating base-href, adding hash-routing, and update splash screen

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import {ApplicationConfig, provideBrowserGlobalErrorListeners} from '@angular/core';
-import {provideRouter} from '@angular/router';
+import {provideRouter, withHashLocation} from '@angular/router';
 
 import {routes} from './app.routes';
 import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
@@ -22,6 +22,6 @@ import {provideClientHydration, withEventReplay} from '@angular/platform-browser
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes, withHashLocation()), provideClientHydration(withEventReplay())
   ]
 };
